88f1c9ebe59d31e7c553b1b0ea1d3981be340667
ChenPP-123 <chenpp-123@users.noreply.github.com>
2026-05-23 23:11:42 +0800

    add a computer version of guess number

---
+++ guess_the_number for computer.py
@@ -0,0 +1,55 @@
+import random as r
+
+
+def get_enter():
+    while True:
+        try:
+            answer = int(
+                input("Enter a number between 1 and 100 for computer to guess!:")
+            )
+            if 1 <= answer <= 100:
+                return answer
+            else:
+                print("please make sure the number is between 1 and 100!")
+        except ValueError:
+            print("Invalid input!")
+
+
+def get_response(computer_guess):
+    while True:
+        response = input(
+            f"the computer's answer is {computer_guess}, is it too low, too high or correct?:"
+        ).lower()
+        if response in ("too low", "too high", "correct"):
+            return response
+        else:
+            print("Invalid input!")
+
+
+answer = get_enter()
+
+high = 100
+low = 1
+
+while True:
+    computer_guess = r.randint(low, high)
+
+    print(computer_guess)
+    response = get_response(computer_guess)
+
+    match response:
+        case "too low":
+            if computer_guess < answer:
+                low = computer_guess + 1
+            else:
+                print("Hey! Your responses are contradictory. Are you cheating?")
+                break
+        case "too high":
+            if computer_guess > answer:
+                high = computer_guess - 1
+            else:
+                print("Hey! Your responses are contradictory. Are you cheating?")
+                break
+        case "correct":
+            print(f"answer is {computer_guess}!")
+            break
