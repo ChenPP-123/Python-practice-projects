529ef628ca07edd7f43b21ea301976faeb50b1a2
ChenPP-123 <chenpp-123@users.noreply.github.com>
2026-05-26 22:34:44 +0800

    add a visual dict for hangman game

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+from hangman_visual import lives_visual_dict
 import string
 
 
@@ -10,14 +11,22 @@ def get_valid_word(words):
     return word.upper()
 
 
-def hangman():
+def hangman(lives: int = 7):
+
     word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    while len(word_letters) > 0:
-        print("You have used these letters:", " ".join(used_letters))
+    while len(word_letters) > 0 and lives > 0:
+        print("-" * 100)
+        print(
+            "You have",
+            lives,
+            "lives,and you have used these letters:",
+            " ".join(used_letters),
+        )
+        print(lives_visual_dict[lives])
         word_list = [letter if letter in used_letters else "-" for letter in word]
         print("Current word:", " ".join(word_list))
 
@@ -26,15 +35,22 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                print("This letter is not in the word")
+                lives -= 1
+
         elif user_letter in used_letters:
             print("You have already used that one, please try anotherone.")
         else:
             print("Invalid charactor, please try again.")
 
-    print(
-        "The word is :",
-        " ".join([letter if letter in used_letters else "-" for letter in word]),
-    )
+    if len(word_letters) == 0:
+        print(
+            "The word is :",
+            " ".join([letter if letter in used_letters else "-" for letter in word]),
+        )
+    else:
+        print("You die, the word is", word)
 
 
 hangman()
